bfee84c7aeb256d4bd6412ea034aa21102de5615
kahfinur <iqbalnurkahfi29@gmail.com>
2025-08-19 22:41:46 +0700

    Delete datawarga directory
    hapus test

--- datawarga/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post, AuthPassword, Warga
-
-admin.site.register(Post)
-
-# Register your models here.
-
-admin.site.register(AuthPassword)
-admin.site.register(Warga)
